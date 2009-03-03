0ef6912978dcafbc9f826b34d337fa178120d893
Christopher R. Murphy <chmurph2+git@gmail.com>
2009-03-03 17:31:59 -0500

    Added tests for parsing the table of contents.
    Note: Renamed parse_toc.rb to parse_table_of_contents.rb.

---
+++ test/parse_table_of_contents_test.rb
@@ -0,0 +1,34 @@
+# require "#{File.dirname(__FILE__)}/test_helper"
+require "test/unit"
+require "../parse_table_of_contents"
+
+class ParseTableOfContentsTest < Test::Unit::TestCase
+  def setup
+    @essays = parse
+  end
+
+  def test_should_return_an_array_of_arrays
+    assert_kind_of Array, @essays
+    assert_kind_of Array, @essays[0]
+  end
+
+  def test_should_parse_92_essays
+    assert_equal 92, @essays.size
+  end
+
+  def test_should_return_expected_chapter_title_and_chapter_number_and_essay_title_for_the_first_essay
+    assert_equal "Introduction (Chapter 1): What is Getting Real?", @essays[0][0]
+  end
+
+  def test_should_return_uri_for_the_first_essay
+    assert_equal "ch01_What_is_Getting_Real.php", @essays[0][1]
+  end
+
+  def test_should_return_expected_chapter_title_and_chapter_number_and_essay_title_for_the_last_essay
+    assert_equal "Conclusion (Chapter 16): 37signals Resources", @essays[91][0]
+  end
+
+  def test_should_return_uri_for_the_last_essay
+    assert_equal "ch16_37signals_Resources.php", @essays[91][1]
+  end
+end
